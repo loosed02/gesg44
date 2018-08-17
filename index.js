27d7ac31ab7ae6ff985cbbbc3e8fd1cecba10abf
MrZeke <30325217+charlier147@users.noreply.github.com>
2018-08-17 17:59:05 -0400

    3
    3

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const client = new Discord.Client({
 });
 client.on("message", message =>{
        if(message.author.bot) return;
-       if(message.content.indexOf(auth.prefix) !== 0) return;
+       if(message.content.indexOf(config.prefix) !== 0) return;
        const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
        const command = args.shift().toLowerCase();
        try {
