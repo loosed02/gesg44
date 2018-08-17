68d53147b750d5ed42a95e0ae7d47254088b8bc0
MrZeke <30325217+charlier147@users.noreply.github.com>
2018-08-17 17:48:20 -0400

    8
    8

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
 const Discord = require(`discord.js`);
-const auth = require(`./settings/config.json`)
-
 const fs = require(`fs`);
 const client = new Discord.Client({
     disableEveryone: false,
     autoReconnect: true
 });
-    //Command Handler
-    client.commands = new Discord.Collection();
-    fs.readdir("./commands/", (err, files) => {
-      if(err) console.log(err);
-      let jsfile = files.filter(f => f.split(".").pop() === "js")
-      if(jsfile.length <= 0){
-        console.log("Couldn't find commands.");
-        return;
-      }
-        jsfile.forEach((f, i) =>{
-        let props = require(`./commands/${f}`);
-        console.log(`${f} loaded!`);
-        client.commands.set(props.help.name, props);
-      });
-    })
-      //Prefix handler
-      client.on("message", async message => {
-      if(message.author.bot) return;
-      if(message.channel.type === "dm") return;
-    
-      let prefix = auth.prefix;
-      let messageArray = message.content.split(" ");
-      let cmd = messageArray[0];
-      let args = messageArray.slice(1);
-      let commandfile = client.commands.get(cmd.slice(prefix.length));
-      if(commandfile) commandfile.run(client,message,args);
-    
-    });
+client.on("message", message => {
+    if (message.author.bot) return;
+    if(message.content.indexOf(config.prefix) !== 0) return;
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    try {
+      let commandFile = require(`./commands/${command}.js`);
+      commandFile.run(client, message, args);
+    } catch (err) {
+      console.error(err);
+    }
+  });
 client.on('ready', () => {
     client.user.setActivity(`${client.users.size} users`, { type: "WATCHING"});
     //Status
